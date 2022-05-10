5bfcb6c3cb32f3d0141ac647696404de0fc073e8
Don Svederus <svederus@gmail.com>
2022-05-09 22:36:07 -0600

    0.023 Variables

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,9 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, Don Svederus");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+        // Shortcut: sout = (System.out.println)
     }
 }
