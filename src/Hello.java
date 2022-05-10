9c38e22d50daa6ab4ae7affb9c9e2a035b333f4a
Don Svederus <svederus@gmail.com>
2022-05-09 22:47:49 -0600

    0.024 Starting out with Expressions

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,13 @@ public class Hello {
         System.out.println("Hello, Don Svederus");
 
         int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
         // Shortcut: sout = (System.out.println)
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
